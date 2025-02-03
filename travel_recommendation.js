33b665ca02200827a9b98ffb1c984ca30569b118
DapKr <daphnaz129@gmail.com>
2025-02-03 23:09:34 +0200

    Update travel_recommendation.js

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -60,11 +60,7 @@ function searchLocation() {
                     country.cities.forEach(city => {
                         foundResults = true;
                         const { formattedDate, formattedTime } = getLocalTime(city.timeZone);
-                        createResultDiv(
-                            resultContainer,
-                            city.imageUrl,
-                            `<h3>${city.name}</h3><p>${city.description}</p><p>${formattedDate} ${formattedTime}</p>`
-                        );
+                        createResultDiv(resultContainer, city.imageUrl, `<h3>${city.name}</h3><p>${city.description}</p><p>${formattedDate} ${formattedTime}</p>`);
                     });
                 });
             }); else {
